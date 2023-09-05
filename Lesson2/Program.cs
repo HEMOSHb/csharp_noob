c2d59f487ccfbb9731abb0b31b5682b0791e0ca4
Nikita <Hemoshb@gmail.com>
2023-09-05 21:52:28 +0300

    Выполнили задачу №2

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -28,3 +28,29 @@
 //     int number1 = firstDigit*10 + thirdDigit;
 //     System.Console.WriteLine(number1);
 // }
+
+// 12. Напишите программу, которая будет принимать
+// на вход два числа и выводить, является ли второе
+// число кратным первому. Если число 2 не кратно числу
+// 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+// Console.Write("Введите число A = ");
+// int A = int.Parse(Console.ReadLine());
+// Console.Write("Введите число B = ");
+// int B = int.Parse(Console.ReadLine());
+// void Multiply(int A, int B)
+// {
+//     if (B % A == 0)
+//     {
+//         Console.Write("Кратно");
+//     }
+//     else
+//     {
+//         int remainder = B % A;
+//         Console.Write($"Не кратно,{remainder}");
+//     }
+// }
+// Multiply(A,B);
+
