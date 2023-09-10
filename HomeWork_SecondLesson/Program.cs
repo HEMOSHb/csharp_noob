7126a43e418591d3e161f195ae850f09c2850b15
Nikita <Hemoshb@gmail.com>
2023-09-10 14:12:32 +0300

    Внесли маленькие правки в коде с методом promt

--- HomeWork_SecondLesson/Program.cs
+++ HomeWork_SecondLesson/Program.cs
@@ -17,22 +17,22 @@
 // }
 
 // Второй способ решения задачи №10 с помощью метода promt.
-int Promt (string message)
-{
-    System.Console.Write(message);
-    string value=Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
-}
-int number = Promt("Введите трехзначное число > ");
-if (number < 100 || number >= 1000)
-{
-    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
-    return;
-}
-Console.WriteLine($"Введенное число `{number}`");
-int secondRank = number / 10% 10;
-Console.WriteLine($"Вторая цифра `{secondRank}`");
+// int Promt (string message)
+// {
+//     System.Console.Write(message);
+//     string value=Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
+// int number = Promt("Введите трехзначное число > ");
+// if (number < 100 || number >= 1000)
+// {
+//     Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
+//     return;
+// }
+// Console.WriteLine($"Введенное число `{number}`");
+// int secondRank = number / 10% 10;
+// Console.WriteLine($"Вторая цифра `{secondRank}`");
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
