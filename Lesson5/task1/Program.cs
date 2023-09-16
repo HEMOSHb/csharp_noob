aa856bd257d07d7537e15034793abce6ba31fd7f
Nikita <Hemoshb@gmail.com>
2023-09-16 12:33:08 +0300

    Порешали задачи с массивами на 5 семинаре

---
+++ Lesson5/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿int [] array = {-4,-8,8,2};
+int[] reArray = new int [4];
+for (int i = 0; i<array.Length; i++)
+{
+    
+    reArray[i]=array[i] * -1;
+    
+}
+Console.Write("Вывод массива [ ");
+for (int i=0; i<array.Length; i++)
+{
+    Console.Write($"{array[i]} ","");
+}
+Console.Write("]");
+Console.ReadLine();
+
+
+
+// int[] array = {-4, -8, 8, 2};
+// int[] resArray = new int[4];
+// for (int i = 0; i < array.Length; i++)
+// {
+    
+    
+//     resArray[i] = array[i] * -1;
+    
+    
+// }
+// string resArray2 = String.Join(", ", resArray);
+// Console.WriteLine(resArray2);
---
+++ Lesson5/task2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 12 10
+Только для нечетного количества элементов массива
+
+int [] arr = new int [] {1,2,3,4,5,6};
+int [] newArr = Multiplication(arr);
+Console.Write(string. Join("," , newArr));
+
+int[] Multiplication(int[] array)
+{
+    int Size;
+    if (array.Length % 2 == 0) Size = array.Length/2;
+    else Size = array.Length/2 + 1;
+    int [] newArray = new int [Size];
+    for (int i = 0; i<newArray.Length; i++)
+    {
+        if (i==newArray.Length - 1) newArray[i] = array[i];
+        else newArray[i] = array[i]*array[array.Length - i - 1];
+    }
+    return newArray;
+}
+
+Универсальный метод для четного и нечетного количества элементов массива
+
+// int[] arr = new int[] {1, 2, 3, 4, 5};
+// int[] newArr = Multiplication(arr);
+// Console.WriteLine(string.Join(",", newArr));
+
+// int[] Multiplication(int[] array)
+// {
+//     int size;
+//     if(array.Length % 2 == 0) size = array.Length/2;
+//     else size = array.Length /2 + 1;
+//     int[] newArray = new int[size];
+//     for (int i = 0; i < newArray.Length; i++)
+//     {
+//         if(array.Length % 2 == 1 && i == newArray.Length - 1) newArray[i] = array[i];
+//         else newArray[i] = array[i] * array[array.Length - i - 1];
+//     }
+//     return newArray;
+// }
